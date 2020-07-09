f756e9bffc18daaee030059877d528aacfb27803
fabrizio3000 <saavefabri@gmail.com>
2020-07-09 01:00:35 -0300

    nuebo cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	alert("responde el sigiente mensage");
+	prompt("¿te gusta programar?")
+
 }
 
